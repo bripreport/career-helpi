f2974c926faf6a3ecb845f711c0b146e214dc5c1
Dan Mihovch <dmihovch@gmail.com>
2025-05-13 14:34:04 -0400

    polish

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,10 +1,10 @@
 import "../styles/loading.css";
-import sassy from "../assets/sassy.jpg";
+import sassy from "../assets/ChatGPT_Image_May_13__2025__02_28_35_PM-removebg-preview.png";
 
 function Loading() {
   return (
     <>
-      <h1>Loading...</h1>
+      <h1 id="loading-text">Loading...</h1>
       <img className="loading" src={sassy} alt="loading..." />
     </>
   );
